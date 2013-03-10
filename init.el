4245959a9ffe92c35724e0948abfda96b0290e51
MindMirror <lawrence@newbit.io>
2013-03-11 00:40:56 +0800

    Update init.el file

--- init.el
+++ init.el
@@ -2,3 +2,17 @@
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add package list here
+(defvar my-packages '(clojure-mode
+		      paredit
+		      nrepl
+		      magit)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
