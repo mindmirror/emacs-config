0631393761c3ac4d135a1c2f47cb3b61f696a55e
MindMirror <lawrence@newbit.io>
2013-03-30 23:36:34 +0800

    Remove the customized variables

--- init.el
+++ init.el
@@ -33,15 +33,3 @@
 (mapc 'load (directory-files module-dir nil "^[^#].*el$"))
 (load "server")
 (unless (server-running-p) (server-start))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("1df4f61bb50f58d78e88ea75fb8ce27bac04aef1032d4ea6dafe4667ef39eb41" "936e5cac238333f251a8d76a2ed96c8191b1e755782c99ea1d7b8c215e66d11e" "843a82ff3b91bec5430f9acdd11de03fc0f7874b15c1b6fbb965116b4c7bf830" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
