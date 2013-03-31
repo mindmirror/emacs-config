a4fb1b897baa9c5ba5960bfe39cbcc4ecccb6818
MindMirror <lawrence@newbit.io>
2013-03-31 23:58:51 +0800

    Add el-get setup.

--- init.el
+++ init.el
@@ -25,6 +25,18 @@
   (when (not (package-installed-p p))
     (package-install p)))
 
+;; Set up el-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(el-get 'sync)
+
 ;; Load customizing files
 (defvar emacs-dir (file-name-directory load-file-name)
   "top level emacs dir")
