2533a70544e8264f6e16a6715b4d9491ef0f0bd1
Lawrence QIN <lawrence.qin@pixels.asia>
2014-11-11 11:05:08 +0800

    Update init.el file

--- init.el
+++ init.el
@@ -1,3 +1,21 @@
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+(require 'init-benchmarking) ;; Measure startup time
+
 (require 'cask "~/cask/cask.el")
 (cask-initialize)
 (require 'pallet)
+
+(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
+(defconst *is-a-mac* (eq system-type 'darwin))
+
+(add-hook 'after-init-hook
+	  (lambda ()
+	    (message "init completed in %.2fms"
+		     (sanityinc/time-subtract-millis after-init-time before-init-time))))
+
+(provide 'init)
+
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
