71df059feeadc8bff410962e6bf274907560b978
Lawrence QIN <lawrence.qin@pixels.asia>
2014-11-12 08:55:08 +0800

    Add init-utils extension

--- init.el
+++ init.el
@@ -8,6 +8,11 @@
 (defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
 (defconst *is-a-mac* (eq system-type 'darwin))
 
+;;----------------------------------------------------------------------------
+;; Bootstrap config
+;;----------------------------------------------------------------------------
+(require 'init-utils)  ;; Some handy utils
+
 (add-hook 'after-init-hook
 	  (lambda ()
 	    (message "init completed in %.2fms"
