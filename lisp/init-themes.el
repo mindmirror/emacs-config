cb8c6eff644476c16ac58a0564a130f55e636f65
MindMirror <lawrence@newbit.io>
2014-12-26 16:52:48 +0800

    Use tomorrow-night-briwght theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,12 +1,15 @@
-(load-theme 'flatland t)
-;(load-theme 'spacegray t)
+(add-to-list 'custom-theme-load-path (expand-file-name "themes" user-emacs-directory))
+(add-to-list 'load-path (expand-file-name "themes" user-emacs-directory))
+(load-theme 'tomorrow-night-bright t)
+
+(set-face-attribute 'default nil :height 140)
 
 ;;------------------------------------------------------------------------------
 ;; New-style theme support, in which per-frame theming is not possible
 ;;------------------------------------------------------------------------------
 
 ;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(flatland))
+(setq-default custom-enabled-themes '(tomorrow-night-bright))
 
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
